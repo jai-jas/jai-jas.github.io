b899d816bf7c25871c8cb7d265bd3a5f5c8dd23b
jai-jas <jja808310@gmail.com>
2024-12-12 12:17:38 +0530

    Added website files

---
+++ script.js
@@ -0,0 +1,5 @@
+// Simple interactive behavior for the button
+document.getElementById('interact-btn').addEventListener('click', function () {
+    const messageElement = document.getElementById('message');
+    messageElement.textContent = "You've interacted with the Proof of Jogi!";
+});
